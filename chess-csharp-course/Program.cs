19fb92f1e4c8cabdd5326b360e7a7bb864e71ca3
Paulo H Sousa <phsaraujo54@gmail.com>
2020-12-12 11:19:19 -0300

    Project startup

---
+++ chess-csharp-course/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace chess_csharp_course {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
